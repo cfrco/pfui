2cf5c9207e8b682f8845936dea875edc3a34f67f
CatCfrco <z82206.cat@gmail.com>
2013-06-05 19:33:28 +0800

    add : demo/enhance

---
+++ demo/enhance.py
@@ -0,0 +1,17 @@
+import pfui
+import numpy as np
+import scipy.ndimage as scii
+
+#open
+im = pfui.PfImage("../lena.png")
+im.origin()
+
+#render
+pfrender_highpass = lambda im,ins : pfui.qresize(im,pfui.qimg(ins._origin.astype(np.int)-ins._rgb))
+pfrender_enhance = lambda im,ins : pfui.qresize(im,pfui.qimg(ins._origin.astype(np.int)*2-ins._rgb))
+
+#window
+im.window((3,1),(768,256),[["RGB"],[pfrender_highpass],[pfrender_enhance]])
+
+#im.fft.do(scii.fourier_gaussian,4)
+
