2def11d6fb5dc0c6ede9998b999b9fbb5041fa18
cat <cat@cat-ThinkPad-X220.(none)>
2013-06-06 17:06:56 +0800

    merge cfg's pfaudio

---
+++ pfui/pfaudio.py
@@ -0,0 +1,41 @@
+import numpy as np
+import scikits.audiolab as aulab
+import augtk
+
+class PfAudio(object):
+    def __init__(self,filename,mode='r',format=None,channels=0,samplerate=0):
+        if 'w' in mode:
+            if '.wav' in filename:
+                format = aulab.Format()
+            elif '.ogg' in filename:
+                format = aulab.Format(type="ogg",encoding="vorbis",endianness="file")
+
+        self.snd = aulab.Sndfile(filename,mode=mode,format=format,\
+                                 channels=channels,samplerate=samplerate)
+        self.freq = self.snd.samplerate
+
+        if 'w' not in mode:
+            self.frames = self.snd.nframes
+            self.data = self.snd.read_frames(self.frames)
+            self.drawdata = None
+            if self.snd.channels != 1:
+                self.data = self.data[:,0]
+
+        self.av,self.window = None,None
+        print self.snd
+        
+    def play(self,start=0,frames=0):
+        if frames == 0:
+            frames = self.frames
+        aulab.play(self.data[start:frames],fs=self.freq)
+    
+    def view(self,width=1000,height=400):
+        return augtk.AudioView(self,width,height,False)
+
+    def view_all(self,width=1000,height=400):
+        return augtk.AudioView(self,width,height)
+
+    def edit(self,d):
+        self.av = augtk.refresh(self.window,self.av,d)
+        return augtk.AudioView(self,width,height,True)
+
